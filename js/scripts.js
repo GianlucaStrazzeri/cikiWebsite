614ed19992d0a615a9af559c309a9b46d403d4f6
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-01-01 16:29:18 +0100

    Añadido js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+console.log("conectando vainas")
+
+let insertarPrecio=document.getElementById("insertarPrecio");
+let areaPrecio= document.getElementById("areaPrecio");
+
+let insertarPreciop3=document.getElementById("insertarPreciop3");
+let areaPreciop3= document.getElementById("areaPreciop3");
+
+let insertarPreciop4=document.getElementById("insertarPreciop4");
+let areaPreciop4= document.getElementById("areaPreciop4");
+
+insertarPrecio.addEventListener('mouseover', function () {
+    insertarPrecio.innerHTML = '2,59';
+  });
+
+  insertarPreciop3.addEventListener('mouseover', function () {
+    insertarPreciop3.innerHTML = '2,59';
+  });
+  insertarPreciop4.addEventListener('mouseover', function () {
+    insertarPreciop4.innerHTML = '2,59';
+  });
+
+
